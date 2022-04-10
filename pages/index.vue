14b0ea85d544d2bf1810a3057eb5ad0c7605b61d
yucie <yucie@chocolat.local>
2022-04-11 01:25:30 +0900

    modify index

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,21 @@
+<!-- テンプレート -->
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt_app
-      </h1>
-      <h2 class="subtitle">
-        My wicked Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    {{message}}
   </div>
 </template>
+
+<!-- スクリプト -->
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Hello Nuxt!'
+    }
+  }
+}
+</script>
+
+<!-- スタイル -->
+<style>
+</style>
